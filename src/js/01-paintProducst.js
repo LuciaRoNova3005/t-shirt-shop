bdc5e32c9ba0f9ac2dfd6b436424279ed73687eb
LuciaRoNova3005 <lucia.r.nova@gmail.com>
2021-07-29 11:46:39 +0200

    Add styles at products

--- src/js/01-paintProducst.js
+++ src/js/01-paintProducst.js
@@ -27,7 +27,7 @@ const getProductHtmlCode = (product) => {
   let htmlCode = "";
   htmlCode += `<article class="card">`;
   htmlCode += `  <img src="${product.imageUrl}" class="card__img" alt="Producto: ${product.name}">`;
-  htmlCode += `  <h3 class="card__title">${product.name}</h3>`;
+  htmlCode += `  <h3 class="card__title3">${product.name}</h3>`;
   htmlCode += `  <p class="card__description">${product.price} €</p>`;
   htmlCode += `  <button class="js-add-product card__btn" data-id="${product.id}">Añadir a la cesta</button>`;
   htmlCode += `</article>`;
